52412ab023605ea584bc0edf12d15d86dac9b6a4
Espen1997 <eslau1045@hvl.no>
2025-10-03 15:53:10 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -2,51 +2,90 @@ package no.hvl.dat100.matriser;
 
 public class Matriser {
 
-	// a)
-	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-	}
+    public static void skrivUt(int[][] matrise) {
+        if (matrise == null) {
+            System.out.println("Matrisen er null.");
+            return;
+        }
+        for (int[] rad : matrise) {
+            for (int verdi : rad) {
+                System.out.print(verdi + " ");
+            }
+            System.out.println();
+        }
+    }
 
-	// b)
-	public static String tilStreng(int[][] matrise) {
+    public static String tilStreng(int[][] matrise) {
+        if (matrise == null) {
+            return "null";
+        }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
-	}
+        String streng = "[";
+        for (int i = 0; i < matrise.length; i++) {
+            streng += "[";
+            for (int j = 0; j < matrise[i].length; j++) {
+                streng += matrise[i][j];
+                if (j < matrise[i].length - 1) {
+                    streng += ", ";
+                }
+            }
+            streng += "]";
+            if (i < matrise.length - 1) {
+                streng += ", ";
+            }
+        }
+        streng += "]";
+        return streng;
+    }
 
-	// c)
-	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
-	}
+    public static int[][] skaler(int tall, int[][] matrise) {
+        if (matrise == null) return null;
+        int rader = matrise.length, kolonner = matrise[0].length;
+        int[][] resultat = new int[rader][kolonner];
+        for (int i = 0; i < rader; i++) {
+            for (int j = 0; j < kolonner; j++) {
+                resultat[i][j] = tall * matrise[i][j];
+            }
+        }
+        return resultat;
+    }
 
-	// d)
-	public static boolean erLik(int[][] a, int[][] b) {
+    public static boolean erLik(int[][] a, int[][] b) {
+        if (a == b) return true;
+        if (a == null || b == null || a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].length != b[i].length) return false;
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] != b[i][j]) return false;
+            }
+        }
+        return true;
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
-	}
-	
-	// e)
-	public static int[][] speile(int[][] matrise) {
+    public static int[][] speile(int[][] matrise) {
+        if (matrise == null) return null;
+        int rader = matrise.length, kolonner = matrise[0].length;
+        int[][] resultat = new int[kolonner][rader];
+        for (int i = 0; i < rader; i++) {
+            for (int j = 0; j < kolonner; j++) {
+                resultat[j][i] = matrise[i][j];
+            }
+        }
+        return resultat;
+    }
 
-		// TODO
-
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
-	}
-
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
-	}
+    public static int[][] multipliser(int[][] a, int[][] b) {
+        if (a == null || b == null) return null;
+        int raderA = a.length, kolonnerA = a[0].length, raderB = b.length, kolonnerB = b[0].length;
+        if (kolonnerA != raderB) return null;
+        int[][] resultat = new int[raderA][kolonnerB];
+        for (int i = 0; i < raderA; i++) {
+            for (int j = 0; j < kolonnerB; j++) {
+                for (int k = 0; k < kolonnerA; k++) {
+                    resultat[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return resultat;
+    }
 }
